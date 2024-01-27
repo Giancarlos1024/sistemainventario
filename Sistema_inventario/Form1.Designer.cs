c400e05e6d4f320722440ce06ae5ea894162ddd2
Giancarlos1024 <giancarlosvlsqz@gmail.com>
2024-01-27 15:35:47 -0500

    Agregar archivos de proyecto.

---
+++ Sistema_inventario/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace Sistema_inventario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.txtNOMBRE = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCANTIDAD = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPRECIO = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTOTAL = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Cantidad = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Precio = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Total = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(84, 81);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(84, 363);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.Nombre,
+            this.Cantidad,
+            this.Precio,
+            this.Total});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(84, 158);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(424, 189);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // txtNOMBRE
+            // 
+            this.txtNOMBRE.Location = new System.Drawing.Point(218, 81);
+            this.txtNOMBRE.Name = "txtNOMBRE";
+            this.txtNOMBRE.Size = new System.Drawing.Size(100, 20);
+            this.txtNOMBRE.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre";
+            // 
+            // txtCANTIDAD
+            // 
+            this.txtCANTIDAD.Location = new System.Drawing.Point(355, 81);
+            this.txtCANTIDAD.Name = "txtCANTIDAD";
+            this.txtCANTIDAD.Size = new System.Drawing.Size(100, 20);
+            this.txtCANTIDAD.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(352, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Cantidad";
+            // 
+            // txtPRECIO
+            // 
+            this.txtPRECIO.Location = new System.Drawing.Point(490, 81);
+            this.txtPRECIO.Name = "txtPRECIO";
+            this.txtPRECIO.Size = new System.Drawing.Size(100, 20);
+            this.txtPRECIO.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(487, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Precio";
+            // 
+            // txtTOTAL
+            // 
+            this.txtTOTAL.Location = new System.Drawing.Point(630, 81);
+            this.txtTOTAL.Name = "txtTOTAL";
+            this.txtTOTAL.Size = new System.Drawing.Size(100, 20);
+            this.txtTOTAL.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(627, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Total";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(175, 363);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 12;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(268, 363);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 13;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 32;
+            // 
+            // Nombre
+            // 
+            this.Nombre.Text = "Nombre";
+            this.Nombre.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Nombre.Width = 89;
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.Text = "Cantidad";
+            this.Cantidad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Cantidad.Width = 100;
+            // 
+            // Precio
+            // 
+            this.Precio.Text = "Precio";
+            this.Precio.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Precio.Width = 100;
+            // 
+            // Total
+            // 
+            this.Total.Text = "Total";
+            this.Total.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Total.Width = 100;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(515, 158);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 14;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(515, 199);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 15;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.txtTOTAL);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPRECIO);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCANTIDAD);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNOMBRE);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtNOMBRE;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCANTIDAD;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPRECIO;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTOTAL;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Nombre;
+        private System.Windows.Forms.ColumnHeader Cantidad;
+        private System.Windows.Forms.ColumnHeader Precio;
+        private System.Windows.Forms.ColumnHeader Total;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Sistema_inventario/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_inventario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text != "" && txtNOMBRE.Text != "" && txtCANTIDAD.Text != "" && txtPRECIO.Text != "" && txtTOTAL.Text != "")
+            {
+                ListViewItem lista = new ListViewItem(txtID.Text);
+                lista.SubItems.Add(txtNOMBRE.Text);
+                lista.SubItems.Add(txtCANTIDAD.Text);
+                lista.SubItems.Add(txtPRECIO.Text);
+                lista.SubItems.Add(txtTOTAL.Text);
+                listView1.Items.Add(lista);
+
+                txtID.Text = "";
+                txtNOMBRE.Text = "";
+                txtCANTIDAD.Text = "";
+                txtPRECIO.Text = "";
+                txtTOTAL.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Por favor, Termine de llenar todos los datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // Obtener el elemento seleccionado
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+
+                if (txtID.Text != "" && txtNOMBRE.Text != "" && txtCANTIDAD.Text != "" && txtPRECIO.Text != "" && txtTOTAL.Text != "")
+                {
+                    // Modificar los valores del elemento con los nuevos valores de los TextBox
+                    selectedItem.SubItems[0].Text = txtID.Text;
+                    selectedItem.SubItems[1].Text = txtNOMBRE.Text;
+                    selectedItem.SubItems[2].Text = txtCANTIDAD.Text;
+                    selectedItem.SubItems[3].Text = txtPRECIO.Text;
+                    selectedItem.SubItems[4].Text = txtTOTAL.Text;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona un elemento para actualizar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem lista in listView1.SelectedItems) 
+            {
+                lista.Remove();
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
